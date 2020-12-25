a7985484e379f5f8fe23ae631a173677b4fe207b
SultanovSamat <sultanovsamat89@mail.ru>
2020-12-25 09:44:14 +0600

    Add files via upload

---
+++ XRoadApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using XRoadModels;
+
+namespace XRoadApp
+{
+    static class Program
+    {
+        static void Main()
+        {
+            if (!Environment.UserInteractive)
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new ServiceMain()
+                }; 
+                ServiceBase.Run(ServicesToRun);
+            }
+            else
+            {
+                "Begin".Info();
+                ServiceHost host = null;
+                ServiceHost hostInternal = null;
+                try
+                {
+                    host = new ServiceHost(typeof(XRoadService), Util.ServiceUri);
+                    var smb = new ServiceMetadataBehavior { HttpGetEnabled = true, MetadataExporter = { PolicyVersion = PolicyVersion.Policy15 }, };
+                    host.Description.Behaviors.Add(smb);
+                    host.Open();
+                    host.DisplayServiceInfo();
+
+                    hostInternal = new ServiceHost(typeof(XRoadInternal), Util.InternalUri);
+                    var smbInternal = new ServiceMetadataBehavior { HttpGetEnabled = true, MetadataExporter = { PolicyVersion = PolicyVersion.Policy15 }, };
+                    hostInternal.Description.Behaviors.Add(smbInternal);
+                    hostInternal.Open();
+                    hostInternal.DisplayServiceInfo();
+                    $"Press <Enter> to stop the service.".Info();
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                }
+                finally
+                {
+                    if (host != null)
+                    {
+                        if (host.State != CommunicationState.Faulted)
+                            host.Close();
+                        else
+                            host.Abort();
+                    }
+                    if (hostInternal != null)
+                    {
+                        if (hostInternal.State != CommunicationState.Faulted)
+                            hostInternal.Close();
+                        else
+                            hostInternal.Abort();
+                    }
+                }
+                "End".Info();
+            }
+        }
+    }
+}
---
+++ XRoadApp/ServiceMain.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using XRoadModels;
+
+namespace XRoadApp
+{
+    public class ServiceMain: ServiceBase
+    {
+        private ServiceHost m_svcHost = null;
+        private ServiceHost m_svcInternal = null;
+        public ServiceMain()
+        {
+            ServiceName = nameof(XRoadService);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            "Start service".Info();
+            try
+            {
+                if (m_svcHost != null) m_svcHost.Close();
+                if (m_svcInternal != null) m_svcInternal.Close();
+
+                m_svcHost = GetService(args);
+                m_svcInternal = GetInternal(args);
+            }
+            catch (Exception ex)
+            {
+                ex.Error();
+            }
+        }
+
+        private static ServiceHost GetService(string[] args)
+        {
+            try
+            {
+                var host = new ServiceHost(typeof(XRoadService), Util.ServiceUri);
+                var smb = new ServiceMetadataBehavior
+                {
+                    HttpGetEnabled = true,
+                    MetadataExporter = { PolicyVersion = PolicyVersion.Policy15 },
+                };
+                host.Description.Behaviors.Add(smb);
+                host.Open();
+                host.DisplayServiceInfo();
+                return host;
+            }
+            catch (Exception ex)
+            {
+                ex.Error();
+                return null;
+            }
+        }
+
+        private static ServiceHost GetInternal(string[] args)
+        {
+            try
+            {
+                var uri = Util.InternalUri;
+                var host = new ServiceHost(typeof(XRoadInternal), uri);
+                var smb = new ServiceMetadataBehavior
+                {
+                    HttpGetEnabled = true,
+                    MetadataExporter = { PolicyVersion = PolicyVersion.Policy15 },
+                };
+                host.Description.Behaviors.Add(smb);
+                host.Open();
+                return host;
+            }
+            catch (Exception ex)
+            {
+                ex.Error();
+                return null;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            "Stop service".Info();
+            if (m_svcHost != null)
+            {
+                m_svcHost.Close();
+                m_svcHost = null;
+            }
+
+            if (m_svcInternal != null)
+            {
+                m_svcInternal.Close();
+                m_svcInternal = null;
+            }
+        }
+    }
+}
