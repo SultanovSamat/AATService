7bc1c01c165e69515859481c270eabfa5e6bb47e
SultanovSamat <sultanovsamat89@mail.ru>
2020-12-25 09:47:33 +0600

    Add files via upload

---
+++ XRoadModels/Util/Util.Convert.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using NLog;
+using System.Configuration;
+using System.ComponentModel;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace XRoadModels
+{
+    public static partial class Util
+    {
+        public static DateTime DefaultDate { get; } = new DateTime(1900, 1, 1);
+
+        public static DateTime ToSafeDate(this DateTime date)
+        {
+            return date < DefaultDate ? DefaultDate : date;
+        }
+
+        public static string ToSafeString(this string str)
+        {
+            return string.IsNullOrEmpty(str) ? string.Empty : str;
+        }
+
+        public static DateTime ToDate(this DateTime? dateTime)
+        {
+            var date = dateTime.HasValue ? dateTime.Value.ToSafeDate() : DefaultDate;
+            return date;
+        }
+
+        public static T? ToNullable<T>(this string s) where T : struct
+        {
+            var result = new T?();
+            try
+            {
+                if (!string.IsNullOrEmpty(s) && s.Trim().Length > 0)
+                {
+                    TypeConverter conv = TypeDescriptor.GetConverter(typeof(T));
+                    result = (T)conv.ConvertFrom(s);
+                }
+            }
+            catch(Exception ex)
+            {
+                ex.Error();
+            }
+            return result;
+        }
+
+        public static void SynchronizedInvoke(this ISynchronizeInvoke sync, Action action)
+        {
+            if (!sync.InvokeRequired)
+            {
+                action();
+                return;
+            }
+
+            sync.Invoke(action, new object[] { });
+        }
+
+        public static string GetDisplayDate(this object obj, bool end = false)
+        {
+            var properties = obj.GetType().GetProperties();
+            var result = string.Empty;
+            foreach (var prop in properties)
+            {
+                if (prop.PropertyType == typeof(DateTime?))
+                {
+                    if (end)
+                    {
+                        end = false;
+                        continue;
+                    }
+                    var dd = prop.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+                    result = dd?.Name;
+                }
+            }
+            return result;
+        }
+
+        public static string GetDisplay(this object obj, Type type, int numElem = 0)
+        {
+            var properties = obj.GetType().GetProperties();
+            foreach (var prop in properties)
+            {
+                if (numElem-- > 0) continue;
+                if (prop.PropertyType == type)
+                {
+                    var dd = prop.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+                    return dd?.Name;
+                }
+            }
+            return string.Empty;
+        }
+
+        public static string GetFieldsData(this object response, bool isArray = false)
+        {
+            if (response == null) return string.Empty;
+            var result = string.Empty;
+
+            var baseFields = response.GetType().BaseType.GetProperties();
+
+            foreach (var field in baseFields)
+            {
+                var val = field.GetValue(response);
+                result += $"{field.Name}: {val};{Environment.NewLine}";
+            }
+
+            var fields = response.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            foreach (var field in fields)
+            {
+                var val = field.GetValue(response);
+                if (!field.PropertyType.IsArray)
+                {
+                    result += $"{field.Name}: {val};\t";
+                    if (!isArray)
+                        result += Environment.NewLine;
+                }
+                else
+                {
+                    result += $"{field.Name}: ";
+                    var items = val as Array;
+                    if (items == null || items.Length == 0)
+                    {
+                        result += $"[]{Environment.NewLine}";
+                        continue;
+                    }
+                    result += $"[{items.Length}]{Environment.NewLine}";
+                    foreach (var elem in items)
+                    {
+                        result += $"\t{elem.GetFieldsData(isArray: true)}";
+                        result += $"{Environment.NewLine}\t-------------------------------{Environment.NewLine}";
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static bool IsNullOrEmpty(this Array array)
+        {
+            return (array == null || array.Length == 0);
+        }
+
+    }
+}
---
+++ XRoadModels/Util/Util.Service.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using NLog;
+using System.Configuration;
+using System.ComponentModel;
+using System.ServiceModel;
+using XRoadModels.ExternalService;
+using XRoadStructure;
+using System.ServiceModel.Description;
+using XRoadModels.ws_ExternalSF;
+
+namespace XRoadModels
+{
+    public static partial class Util
+    {
+        private static string ServiceName
+        {
+            get
+            {
+                var name = string.Empty;
+                foreach (var elem in TypeDescriptor.GetAttributes(typeof(IXRoadService)))
+                {
+                    if (elem.GetType() == typeof(ServiceContractAttribute))
+                    {
+                        name = ((ServiceContractAttribute)elem).Name;
+                    }
+                }
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Service contract doesn't have a name!");
+                return name;
+            }
+        }
+
+        private static string InternalName
+        {
+            get
+            {
+                var name = string.Empty;
+                foreach (var elem in TypeDescriptor.GetAttributes(typeof(IXRoadInternal)))
+                {
+                    if (elem.GetType() == typeof(ServiceContractAttribute))
+                    {
+                        name = ((ServiceContractAttribute)elem).Name;
+                    }
+                }
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Service contract doesn't have a name!");
+                return name;
+            }
+        }
+
+        public static Uri ServiceUri
+        {
+            get
+            {
+                return new Uri($"http://{IP}:{Port}/{ServiceName}");
+            }
+        }
+
+        public static Uri InternalUri
+        {
+            get
+            {
+                return new Uri($"http://{IP}:{Port}/{InternalName}");
+            }
+        }
+
+        public static void DisplayServiceInfo(this ServiceHost host)
+        {
+            try
+            {
+                foreach (var endpoint in host.Description.Endpoints)
+                {
+                    var security = endpoint.Binding.GetType().GetProperty(nameof(BasicHttpBinding.Security)).GetValue(endpoint.Binding);
+                    $"{nameof(BasicHttpSecurity.Mode)}: {security.GetType().GetProperty(nameof(BasicHttpSecurity.Mode)).GetValue(security)}".Info();
+                    var message = security.GetType().GetProperty(nameof(BasicHttpSecurity.Message)).GetValue(security);
+                    $"{nameof(BasicHttpMessageSecurity.AlgorithmSuite)}: {message.GetType().GetProperty(nameof(BasicHttpMessageSecurity.AlgorithmSuite)).GetValue(message)}".Info();
+                    $"{nameof(BasicHttpMessageSecurity.ClientCredentialType)}: {message.GetType().GetProperty(nameof(BasicHttpMessageSecurity.ClientCredentialType)).GetValue(message)}".Info();
+                    var transport = security.GetType().GetProperty(nameof(BasicHttpSecurity.Transport)).GetValue(security);
+                    $"{nameof(BasicHttpSecurity.Transport)}.{nameof(HttpTransportSecurity.ClientCredentialType)}: {transport.GetType().GetProperty(nameof(HttpTransportSecurity.ClientCredentialType)).GetValue(transport)}".Info();
+                }
+                $"The service is available at {host.Description.Endpoints[0].Address}".Info();
+            }
+            catch (Exception ex)
+            {
+                ex.Error();
+            }
+        }
+
+        public static ws_ExternalClient WSClient
+        {
+            get
+            {
+                try
+                {
+                    ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslpolicyerrors) => true;
+                    var initClient = new ws_ExternalClient();
+                    initClient.ClientCredentials.UserName.UserName = "admin";
+                    initClient.ClientCredentials.UserName.Password = Util.PasswordHash;
+                    return initClient;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static void SetSecurity()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslpolicyerrors) => true;
+        }
+
+        public static PaymentDebtInfoServiceClient WSClientSF
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    return new PaymentDebtInfoServiceClient();
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalGNSDebt.GnsServicePortTypeClient WSClientGNSDebt
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    return new ws_ExternalGNSDebt.GnsServicePortTypeClient();
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+        public static ws_ExternalGNSReg.GnsServicePortTypeClient WSClientGNSReg
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    return new ws_ExternalGNSReg.GnsServicePortTypeClient();
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalPassport.InfocomServicePortTypeClient WSClientGRSPassport
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    var cl = new ws_ExternalPassport.InfocomServicePortTypeClient();
+                    //cl.Endpoint.Behaviors.Add(new InspectorBehavior());
+                    return cl;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalAddress.InfocomServicePortTypeClient WSClientGRSAddress
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    var initClient = new ws_ExternalAddress.InfocomServicePortTypeClient();
+                    return initClient;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalVehicles.InfocomServicePortTypeClient WSClientGRSVehicle
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    var initClient = new ws_ExternalVehicles.InfocomServicePortTypeClient();
+                    return initClient;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalZags.InfocomServicePortTypeClient WSClientGRSZags
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    return new ws_ExternalZags.InfocomServicePortTypeClient();
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalDriver.InfocomServicePortTypeClient WSClientGRSDriver
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    return new ws_ExternalDriver.InfocomServicePortTypeClient();
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalForeign.InfocomServicePortTypeClient WSClientGRSForeign
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    var initClient = new ws_ExternalForeign.InfocomServicePortTypeClient();
+                    return initClient;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalGTS.MTDServiceClient WSClientGTS
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    var initClient = new ws_ExternalGTS.MTDServiceClient();
+                    return initClient;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalJur.SubjectPortClient WSClientJur
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    var initClient = new ws_ExternalJur.SubjectPortClient();
+                    return initClient;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+
+        public static ws_ExternalMeta.metaServicesPortClient WSClientMeta
+        {
+            get
+            {
+                try
+                {
+                    SetSecurity();
+                    var initClient = new ws_ExternalMeta.metaServicesPortClient();
+                    return initClient;
+                }
+                catch (Exception ex)
+                {
+                    ex.Error();
+                    throw;
+                }
+            }
+        }
+    }
+}
